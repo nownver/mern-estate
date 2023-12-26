daaffd322ccde3ccb31b94a95c82a6bd21233466
nownver <urawee.thani@gmail.com>
2023-12-26 14:01:03 +0700

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   //   console.log(req.body);
@@ -13,7 +14,30 @@ export const signup = async (req, res, next) => {
     res.status(201).json("User created successfully!");
   } catch (error) {
     // res.status(500).json(error.message);
-    next(error)
+    next(error);
     // next(errorHandler(550, 'error from function'));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid Credentials!"));
+    //using id is best practice
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //don't show password to user
+    const {password: pass, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), //time in seconds(expire in 1000 days)
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
